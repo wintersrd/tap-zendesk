b3223b8f0bbcc25cf4be78eebad2572843d150c7
Rob Winters <rwinters@tripactions.com>
2019-11-20 09:49:00 -0800

    Add some retries and error handling for ticket comments and metrics

--- tap_zendesk/streams.py
+++ tap_zendesk/streams.py
@@ -2,6 +2,7 @@ import os
 import json
 import datetime
 import pytz
+import time
 import zenpy
 from zenpy.lib.exception import RecordNotFoundException
 import singer
@@ -12,44 +13,45 @@ from tap_zendesk import metrics as zendesk_metrics
 
 
 LOGGER = singer.get_logger()
-KEY_PROPERTIES = ['id']
+KEY_PROPERTIES = ["id"]
 
 CUSTOM_TYPES = {
-    'text': 'string',
-    'textarea': 'string',
-    'date': 'string',
-    'regexp': 'string',
-    'dropdown': 'string',
-    'integer': 'integer',
-    'decimal': 'number',
-    'checkbox': 'boolean',
+    "text": "string",
+    "textarea": "string",
+    "date": "string",
+    "regexp": "string",
+    "dropdown": "string",
+    "integer": "integer",
+    "decimal": "number",
+    "checkbox": "boolean",
 }
+
 
 def get_abs_path(path):
     return os.path.join(os.path.dirname(os.path.realpath(__file__)), path)
+
 
 def process_custom_field(field):
     """ Take a custom field description and return a schema for it. """
     zendesk_type = field.type
     json_type = CUSTOM_TYPES.get(zendesk_type)
     if json_type is None:
-        raise Exception("Discovered unsupported type for custom field {} (key: {}): {}"
-                        .format(field.title,
-                                field.key,
-                                zendesk_type))
-    field_schema = {'type': [
-        json_type,
-        'null'
-    ]}
+        raise Exception(
+            "Discovered unsupported type for custom field {} (key: {}): {}".format(
+                field.title, field.key, zendesk_type
+            )
+        )
+    field_schema = {"type": [json_type, "null"]}
 
-    if zendesk_type == 'date':
-        field_schema['format'] = 'datetime'
-    if zendesk_type == 'dropdown':
-        field_schema['enum'] = [o['value'] for o in field.custom_field_options]
+    if zendesk_type == "date":
+        field_schema["format"] = "datetime"
+    if zendesk_type == "dropdown":
+        field_schema["enum"] = [o["value"] for o in field.custom_field_options]
 
     return field_schema
 
-class Stream():
+
+class Stream:
     name = None
     replication_method = None
     replication_key = None
@@ -67,36 +69,36 @@ class Stream():
         if value and utils.strptime_with_tz(value) > current_bookmark:
             singer.write_bookmark(state, self.name, self.replication_key, value)
 
-
     def load_schema(self):
         schema_file = "schemas/{}.json".format(self.name)
         with open(get_abs_path(schema_file)) as f:
             schema = json.load(f)
         return self._add_custom_fields(schema)
 
-    def _add_custom_fields(self, schema): # pylint: disable=no-self-use
+    def _add_custom_fields(self, schema):  # pylint: disable=no-self-use
         return schema
 
     def load_metadata(self):
         schema = self.load_schema()
         mdata = metadata.new()
 
-        mdata = metadata.write(mdata, (), 'table-key-properties', self.key_properties)
-        mdata = metadata.write(mdata, (), 'forced-replication-method', self.replication_method)
+        mdata = metadata.write(mdata, (), "table-key-properties", self.key_properties)
+        mdata = metadata.write(mdata, (), "forced-replication-method", self.replication_method)
 
         if self.replication_key:
-            mdata = metadata.write(mdata, (), 'valid-replication-keys', [self.replication_key])
+            mdata = metadata.write(mdata, (), "valid-replication-keys", [self.replication_key])
 
-        for field_name in schema['properties'].keys():
+        for field_name in schema["properties"].keys():
             if field_name in self.key_properties or field_name == self.replication_key:
-                mdata = metadata.write(mdata, ('properties', field_name), 'inclusion', 'automatic')
+                mdata = metadata.write(mdata, ("properties", field_name), "inclusion", "automatic")
             else:
-                mdata = metadata.write(mdata, ('properties', field_name), 'inclusion', 'available')
+                mdata = metadata.write(mdata, ("properties", field_name), "inclusion", "available")
 
         return metadata.to_list(mdata)
 
     def is_selected(self):
         return self.stream is not None
+
 
 def raise_or_log_zenpy_apiexception(schema, stream, e):
     # There are multiple tiers of Zendesk accounts. Some of them have
@@ -105,9 +107,13 @@ def raise_or_log_zenpy_apiexception(schema, stream, e):
     # it doesn't have access.
     if not isinstance(e, zenpy.lib.exception.APIException):
         raise ValueError("Called with a bad exception type") from e
-    if json.loads(e.args[0])['error']['message'] == "You do not have access to this page. Please contact the account owner of this help desk for further help.":
-        LOGGER.warning("The account credentials supplied do not have access to `%s` custom fields.",
-                       stream)
+    if (
+        json.loads(e.args[0])["error"]["message"]
+        == "You do not have access to this page. Please contact the account owner of this help desk for further help."
+    ):
+        LOGGER.warning(
+            "The account credentials supplied do not have access to `%s` custom fields.", stream
+        )
         return schema
     else:
         raise e
@@ -123,13 +129,17 @@ class Organizations(Stream):
         # NB: Zenpy doesn't have a public endpoint for this at time of writing
         #     Calling into underlying query method to grab all fields
         try:
-            field_gen = self.client.organizations._query_zendesk(endpoint.organization_fields, # pylint: disable=protected-access
-                                                                 'organization_field')
+            field_gen = self.client.organizations._query_zendesk(
+                endpoint.organization_fields,  # pylint: disable=protected-access
+                "organization_field",
+            )
         except zenpy.lib.exception.APIException as e:
             return raise_or_log_zenpy_apiexception(schema, self.name, e)
-        schema['properties']['organization_fields']['properties'] = {}
+        schema["properties"]["organization_fields"]["properties"] = {}
         for field in field_gen:
-            schema['properties']['organization_fields']['properties'][field.key] = process_custom_field(field)
+            schema["properties"]["organization_fields"]["properties"][
+                field.key
+            ] = process_custom_field(field)
 
         return schema
 
@@ -139,6 +149,7 @@ class Organizations(Stream):
         for organization in organizations:
             self.update_bookmark(state, organization.updated_at)
             yield (self.stream, organization)
+
 
 class Users(Stream):
     name = "users"
@@ -150,9 +161,11 @@ class Users(Stream):
             field_gen = self.client.user_fields()
         except zenpy.lib.exception.APIException as e:
             return raise_or_log_zenpy_apiexception(schema, self.name, e)
-        schema['properties']['user_fields']['properties'] = {}
+        schema["properties"]["user_fields"]["properties"] = {}
         for field in field_gen:
-            schema['properties']['user_fields']['properties'][field.key] = process_custom_field(field)
+            schema["properties"]["user_fields"]["properties"][field.key] = process_custom_field(
+                field
+            )
 
         return schema
 
@@ -163,6 +176,7 @@ class Users(Stream):
             self.update_bookmark(state, user.updated_at)
             yield (self.stream, user)
 
+
 class Tickets(Stream):
     name = "tickets"
     replication_method = "INCREMENTAL"
@@ -171,6 +185,7 @@ class Tickets(Stream):
     last_record_emit = {}
     buf = {}
     buf_time = 60
+
     def _buffer_record(self, record):
         stream_name = record[0].tap_stream_id
         if self.last_record_emit.get(stream_name) is None:
@@ -202,53 +217,92 @@ class Tickets(Stream):
 
         def emit_sub_stream_metrics(sub_stream):
             if sub_stream.is_selected():
-                singer.metrics.log(LOGGER, Point(metric_type='counter',
-                                                 metric=singer.metrics.Metric.record_count,
-                                                 value=sub_stream.count,
-                                                 tags={'endpoint':sub_stream.stream.tap_stream_id}))
+                singer.metrics.log(
+                    LOGGER,
+                    Point(
+                        metric_type="counter",
+                        metric=singer.metrics.Metric.record_count,
+                        value=sub_stream.count,
+                        tags={"endpoint": sub_stream.stream.tap_stream_id},
+                    ),
+                )
                 sub_stream.count = 0
 
         if audits_stream.is_selected():
             LOGGER.info("Syncing ticket_audits per ticket...")
 
         for ticket in tickets:
-            zendesk_metrics.capture('ticket')
-            generated_timestamp_dt = datetime.datetime.utcfromtimestamp(ticket.generated_timestamp).replace(tzinfo=pytz.UTC)
+            zendesk_metrics.capture("ticket")
+            generated_timestamp_dt = datetime.datetime.utcfromtimestamp(
+                ticket.generated_timestamp
+            ).replace(tzinfo=pytz.UTC)
             self.update_bookmark(state, utils.strftime(generated_timestamp_dt))
 
             ticket_dict = ticket.to_dict()
-            ticket_dict.pop('fields') # NB: Fields is a duplicate of custom_fields, remove before emitting
+            ticket_dict.pop(
+                "fields"
+            )  # NB: Fields is a duplicate of custom_fields, remove before emitting
             should_yield = self._buffer_record((self.stream, ticket_dict))
 
             if audits_stream.is_selected():
                 try:
                     for audit in audits_stream.sync(ticket_dict["id"]):
-                        zendesk_metrics.capture('ticket_audit')
-                        self._buffer_record(audit)
-                except RecordNotFoundException:
-                    LOGGER.warning("Unable to retrieve audits for ticket (ID: %s), " \
-                    "the Zendesk API returned a RecordNotFound error", ticket_dict["id"])
+                        counter = 0
+                        while counter < 3:
+                            try:
+                                zendesk_metrics.capture("ticket_audit")
+                                self._buffer_record(audit)
+                                break
+                            except:
+                                counter += 1
+                                time.sleep(2)
+                except:
+                    LOGGER.warning(
+                        "Unable to retrieve audits for ticket (ID: %s), "
+                        "the Zendesk API returned a RecordNotFound error",
+                        ticket_dict["id"],
+                    )
 
             if metrics_stream.is_selected():
                 try:
                     for metric in metrics_stream.sync(ticket_dict["id"]):
-                        zendesk_metrics.capture('ticket_metric')
-                        self._buffer_record(metric)
-                except RecordNotFoundException:
-                    LOGGER.warning("Unable to retrieve metrics for ticket (ID: %s), " \
-                    "the Zendesk API returned a RecordNotFound error", ticket_dict["id"])
+                        counter = 0
+                        while counter < 3:
+                            try:
+                                zendesk_metrics.capture("ticket_metric")
+                                self._buffer_record(metric)
+                                break
+                            except:
+                                counter += 1
+                                time.sleep(2)
+                except:
+                    LOGGER.warning(
+                        "Unable to retrieve metrics for ticket (ID: %s), "
+                        "the Zendesk API returned a RecordNotFound error",
+                        ticket_dict["id"],
+                    )
 
             if comments_stream.is_selected():
                 try:
                     # add ticket_id to ticket_comment so the comment can
                     # be linked back to it's corresponding ticket
                     for comment in comments_stream.sync(ticket_dict["id"]):
-                        zendesk_metrics.capture('ticket_comment')
-                        comment[1].ticket_id = ticket_dict["id"]
-                        self._buffer_record(comment)
-                except RecordNotFoundException:
-                    LOGGER.warning("Unable to retrieve comments for ticket (ID: %s), " \
-                    "the Zendesk API returned a RecordNotFound error", ticket_dict["id"])
+                        counter = 0
+                        while counter < 3:
+                            try:
+                                zendesk_metrics.capture("ticket_comment")
+                                comment[1].ticket_id = ticket_dict["id"]
+                                self._buffer_record(comment)
+                                break
+                            except:
+                                counter += 1
+                                time.sleep(2)
+                except:
+                    LOGGER.warning(
+                        "Unable to retrieve comments for ticket (ID: %s), "
+                        "the Zendesk API returned a RecordNotFound error",
+                        ticket_dict["id"],
+                    )
 
             if should_yield:
                 for rec in self._empty_buffer():
@@ -265,6 +319,7 @@ class Tickets(Stream):
         emit_sub_stream_metrics(comments_stream)
         singer.write_state(state)
 
+
 class TicketAudits(Stream):
     name = "ticket_audits"
     replication_method = "INCREMENTAL"
@@ -276,6 +331,7 @@ class TicketAudits(Stream):
             self.count += 1
             yield (self.stream, ticket_audit)
 
+
 class TicketMetrics(Stream):
     name = "ticket_metrics"
     replication_method = "INCREMENTAL"
@@ -285,6 +341,7 @@ class TicketMetrics(Stream):
         ticket_metric = self.client.tickets.metrics(ticket=ticket_id)
         self.count += 1
         yield (self.stream, ticket_metric)
+
 
 class TicketComments(Stream):
     name = "ticket_comments"
@@ -296,6 +353,7 @@ class TicketComments(Stream):
         for ticket_comment in ticket_comments:
             self.count += 1
             yield (self.stream, ticket_comment)
+
 
 class SatisfactionRatings(Stream):
     name = "satisfaction_ratings"
@@ -314,6 +372,7 @@ class SatisfactionRatings(Stream):
                 self.update_bookmark(state, satisfaction_rating.updated_at)
                 yield (self.stream, satisfaction_rating)
 
+
 class Groups(Stream):
     name = "groups"
     replication_method = "INCREMENTAL"
@@ -330,6 +389,7 @@ class Groups(Stream):
                 # so we can't save state until we've seen all records
                 self.update_bookmark(state, group.updated_at)
                 yield (self.stream, group)
+
 
 class Macros(Stream):
     name = "macros"
@@ -348,17 +408,19 @@ class Macros(Stream):
                 self.update_bookmark(state, macro.updated_at)
                 yield (self.stream, macro)
 
+
 class Tags(Stream):
     name = "tags"
     replication_method = "FULL_TABLE"
     key_properties = ["name"]
 
-    def sync(self, state): # pylint: disable=unused-argument
+    def sync(self, state):  # pylint: disable=unused-argument
         # NB: Setting page to force it to paginate all tags, instead of just the
         #     top 100 popular tags
         tags = self.client.tags(page=1)
         for tag in tags:
             yield (self.stream, tag)
+
 
 class TicketFields(Stream):
     name = "ticket_fields"
@@ -377,6 +439,7 @@ class TicketFields(Stream):
                 self.update_bookmark(state, field.updated_at)
                 yield (self.stream, field)
 
+
 class TicketForms(Stream):
     name = "ticket_forms"
     replication_method = "INCREMENTAL"
@@ -393,6 +456,7 @@ class TicketForms(Stream):
                 # so we can't save state until we've seen all records
                 self.update_bookmark(state, form.updated_at)
                 yield (self.stream, form)
+
 
 class GroupMemberships(Stream):
     name = "group_memberships"
@@ -414,19 +478,26 @@ class GroupMemberships(Stream):
                     yield (self.stream, membership)
             else:
                 if membership.id:
-                    LOGGER.info('group_membership record with id: ' + str(membership.id) +
-                                ' does not have an updated_at field so it will be syncd...')
+                    LOGGER.info(
+                        "group_membership record with id: "
+                        + str(membership.id)
+                        + " does not have an updated_at field so it will be syncd..."
+                    )
                     yield (self.stream, membership)
                 else:
-                    LOGGER.info('Received group_membership record with no id or updated_at, skipping...')
+                    LOGGER.info(
+                        "Received group_membership record with no id or updated_at, skipping..."
+                    )
+
 
 class SLAPolicies(Stream):
     name = "sla_policies"
     replication_method = "FULL_TABLE"
 
-    def sync(self, state): # pylint: disable=unused-argument
+    def sync(self, state):  # pylint: disable=unused-argument
         for policy in self.client.sla_policies():
             yield (self.stream, policy)
+
 
 STREAMS = {
     "tickets": Tickets,
